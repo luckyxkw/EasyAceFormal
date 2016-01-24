0e9fc2f776f4b33a44cf05757091596f37256f9b
XiaoKangwei <cocolonel.x@gmail.com>
2016-01-24 15:49:50 +0800

    Revert "refactor file name"
    This reverts commit c5a01f182d5f2920c278b563244a6eb9208bc1fa.

--- EasyAce/views.py
+++ EasyAce/views.py
@@ -9,7 +9,7 @@ def getHome(request):
     return HttpResponse(template.render(context))
 
 def getTestimonial(request):
-    template = loader.get_template('for_student.html')
+    template = loader.get_template('testimonial.html')
     context = RequestContext(request,{})
     return HttpResponse(template.render(context))
 
@@ -29,7 +29,7 @@ def getContact(request):
     return HttpResponse(template.render(context))
 
 def getFAQ(request):
-    template = loader.get_template('for_tutor.html')
+    template = loader.get_template('faq.html')
     context = RequestContext(request,{})
     return HttpResponse(template.render(context))
 
@@ -40,7 +40,7 @@ def getHomeZH(request):
     return HttpResponse(template.render(context))
 
 def getTestimonialZH(request):
-    template = loader.get_template('for_student_zh.html')
+    template = loader.get_template('testimonial_zh.html')
     context = RequestContext(request,{})
     return HttpResponse(template.render(context))
 
@@ -60,6 +60,6 @@ def getContactZH(request):
     return HttpResponse(template.render(context))
 
 def getFAQZH(request):
-    template = loader.get_template('for_tutor_zh.html')
+    template = loader.get_template('faq_zh.html')
     context = RequestContext(request,{})
     return HttpResponse(template.render(context))
